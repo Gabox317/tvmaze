58e3faf664f8ff3305639f2eaaadaa208cbc2fcc
Jose Gabriel Bustillos Piñon <jose.bustillos@infotec.mx>
2026-09-17 14:51:37 -0600

    Se crea el servicio para el listado de los shows

---
+++ src/main/java/com/evaluacion/tvmaze/service/ShowService.java
@@ -0,0 +1,48 @@
+package com.evaluacion.tvmaze.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.evaluacion.tvmaze.client.TvMazeClient;
+import com.evaluacion.tvmaze.dto.SearchShowResponseDTO;
+import com.evaluacion.tvmaze.dto.TvMazeShowDTO;
+
+@Service
+public class ShowService {
+	
+	private final TvMazeClient tvMazeClient;
+	
+	public ShowService(TvMazeClient tvMazeClient) {
+		this.tvMazeClient = tvMazeClient;
+	}
+	
+	public List<SearchShowResponseDTO> search(String query){
+		
+		return tvMazeClient.searchShows(query)
+				.stream()
+				.map(item -> {
+					
+					TvMazeShowDTO show = item.show();
+					
+					String channel = null;
+					if(show.network() != null) {
+						channel = show.network().name();
+					} else if (show.webChannel() != null) {
+						channel = show.webChannel().name();
+					}
+					
+					return new SearchShowResponse(
+							show.id(),
+							show.name(),
+							channel,
+							show.summary(),
+							show.genres()
+							);
+							
+				})
+				
+				.toList();
+	}
+
+}
