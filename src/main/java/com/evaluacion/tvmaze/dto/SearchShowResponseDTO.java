ec88c1b6d9e07aa383e2fa0e4bde2537e9d74394
Jose Gabriel Bustillos Piñon <jose.bustillos@infotec.mx>
2026-09-17 14:12:00 -0600

    Se crea DTO de respuesta

---
+++ src/main/java/com/evaluacion/tvmaze/dto/SearchShowResponseDTO.java
@@ -0,0 +1,12 @@
+package com.evaluacion.tvmaze.dto;
+
+import java.util.List;
+
+public record SearchShowResponseDTO(
+		Long id,
+		String name,
+		String channel,
+		String summary,
+		List<String> genres){
+
+}
